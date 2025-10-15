83929acb975fd9b4a2edb5ccb12f5e23233e461a
Omkar Dahifale <omkar.dahifale@acc.ltd>
2025-10-15 12:52:07 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,9 @@ import userRoute from "./routes/userRoute.js"
 import dotenv from "dotenv";
 
 dotenv.config();
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-
 
 app.use("/users", userRoute);
 
